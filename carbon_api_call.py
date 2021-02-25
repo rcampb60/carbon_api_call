34ff82aa65f537ae74be19cca29997a637ae4a5b
Robbie Campbell <robbie.campbell@postcodelottery.co.uk>
2021-02-25 11:27:34 +0000

    Updated script to incoporate a for loop instead of manually retreiving output

--- carbon_api_call.py
+++ carbon_api_call.py
@@ -20,31 +20,11 @@ except:  #If the input isn't in the correct format this section will run, print 
   print("Invalid input, please run again. Input should be a postcode area which is the first half of your postcode.")
   raise SystemExit()
 
-
-#this section picks out each fuel type percentage
-biomass = response_data['data'][0]['data'][0]['generationmix'][0]['perc']
-coal = response_data['data'][0]['data'][0]['generationmix'][1]['perc']
-imports = response_data['data'][0]['data'][0]['generationmix'][2]['perc']
-gas = response_data['data'][0]['data'][0]['generationmix'][3]['perc']
-nuclear = response_data['data'][0]['data'][0]['generationmix'][4]['perc']
-other = response_data['data'][0]['data'][0]['generationmix'][5]['perc']
-hydro = response_data['data'][0]['data'][0]['generationmix'][6]['perc']
-solar = response_data['data'][0]['data'][0]['generationmix'][7]['perc']
-wind = response_data['data'][0]['data'][0]['generationmix'][8]['perc']
-
+generation_mix = response_data['data'][0]['data'][0]['generationmix']
 #this section presents the information requested above and pretties it up for display using f strings
-data_output = f"""
-In the {postcode_upper} postcode area, in {region_name}, the usage of fuels in the last half hour was:
-
-Biomass: {biomass}%
-Coal: {coal}%
-Imports: {imports}%
-Gas: {gas}%
-Nuclear: {nuclear}%
-Other: {other}%
-Hydro: {hydro}%
-Solar: {solar}%
-Wind: {wind}%
-"""
-
+data_output = f"""In the {postcode_upper} postcode area, in {region_name}, the usage of fuels in the last half hour was:"""
 print(data_output)
+
+#this section uses a loop to iterate through the API response. It finds the fuel key, capatilises it and then returns the percentage value and appends it with a percentage sign
+for i in generation_mix:
+    print(f"{i['fuel'].capitalize()}: {i['perc']}%")
